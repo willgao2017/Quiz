b77f7ddd8d6bafd69c78d285ecaf0e68f6bc4dac
will <will.gao2017@outlook.com>
2017-04-25 00:12:18 +0800

    2 questions done

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    int addresult = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,36 @@ public class MainActivity extends AppCompatActivity {
         String a1 = answer1Editable.toString();
 
         if (a1.equals("x")) {
-            score = 100;
-                        return;
+            score += 10;
+        }
+
+        if (addresult==2){
+            score += 10;
         }
 
         Toast.makeText(this, "Score: " + score, Toast.LENGTH_SHORT).show();
+    }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.addresult_1:
+                if (checked)
+                    // Pirates are the best
+                    addresult = 1;
+                break;
+            case R.id.addresult_2:
+                if (checked)
+                    // Ninjas rule
+                    addresult = 2;
+                break;
+            case R.id.addresult_3:
+                if (checked)
+                    //
+                    addresult = 3;
+                break;
+        }
     }
 }
